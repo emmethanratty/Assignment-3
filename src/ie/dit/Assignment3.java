f7752a9b4af8fcadacf36aec6e7da3ec0be3c48c
emmethanratty <c13503217@mydit.ie>
2015-04-09 14:52:09 +0100

    Added Start Background

--- src/ie/dit/Assignment3.java
+++ src/ie/dit/Assignment3.java
@@ -4,13 +4,17 @@ import processing.core.*;
 
 public class Assignment3 extends PApplet{
 	
+	PImage moon;
+	
 	public void setup()
 	{
-		
+		 size(1000,600);
+		 
+		 moon = loadImage("moon.png");
 	}
 
 	public void draw()
 	{
-		
+		background(moon);
 	}
 }
