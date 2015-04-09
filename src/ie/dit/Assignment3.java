9f55b6d829da9e747b03838cfc1e81055dd42916
emmethanratty <c13503217@mydit.ie>
2015-04-09 15:31:24 +0100

    Added Title

--- src/ie/dit/Assignment3.java
+++ src/ie/dit/Assignment3.java
@@ -5,16 +5,20 @@ import processing.core.*;
 public class Assignment3 extends PApplet{
 	
 	PImage moon;
+	PImage title;
 	
 	public void setup()
 	{
 		 size(1000,600);
 		 
 		 moon = loadImage("moon.png");
+		 title = loadImage("Title3.png");
 	}
 
 	public void draw()
 	{
 		background(moon);
+		//imageMode(CENTER);
+		image(title,WIDTH/2 + 200,20,600,200);
 	}
 }
