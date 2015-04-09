34e612c1f9e0b7571a0b99189d9390b904209d0a
emmethanratty <c13503217@mydit.ie>
2015-04-09 16:09:29 +0100

    added start and upgrade

--- src/ie/dit/Assignment3.java
+++ src/ie/dit/Assignment3.java
@@ -6,6 +6,8 @@ public class Assignment3 extends PApplet{
 	
 	PImage moon;
 	PImage title;
+	PImage start;
+	PImage upgrade;
 	
 	public void setup()
 	{
@@ -13,6 +15,8 @@ public class Assignment3 extends PApplet{
 		 
 		 moon = loadImage("moon.png");
 		 title = loadImage("Title3.png");
+		 start = loadImage("Start.png");
+		 upgrade = loadImage("Upgrade.png");
 	}
 
 	public void draw()
@@ -20,5 +24,7 @@ public class Assignment3 extends PApplet{
 		background(moon);
 		//imageMode(CENTER);
 		image(title,WIDTH/2 + 200,20,600,200);
+		image(start,WIDTH/2 + 250,250,400,100);
+		image(upgrade,WIDTH/2 + 250,400,400,100);
 	}
 }
