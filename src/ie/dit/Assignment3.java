fd3d019a20865f94719df946028bd1bb0b191c70
emmethanratty <c13503217@mydit.ie>
2015-04-09 16:50:03 +0100

    added mousepressed to start

--- src/ie/dit/Assignment3.java
+++ src/ie/dit/Assignment3.java
@@ -4,27 +4,88 @@ import processing.core.*;
 
 public class Assignment3 extends PApplet{
 	
+	Start s;
+	
 	PImage moon;
 	PImage title;
 	PImage start;
 	PImage upgrade;
 	
+	char option ='0'; 
+	
+	int menuW = 400;
+	int menuH = 100;
+			
 	public void setup()
 	{
 		 size(1000,600);
+		 
+		 s = new Start(this);
 		 
 		 moon = loadImage("moon.png");
 		 title = loadImage("Title3.png");
 		 start = loadImage("Start.png");
 		 upgrade = loadImage("Upgrade.png");
+		 
 	}
 
 	public void draw()
 	{
-		background(moon);
-		//imageMode(CENTER);
-		image(title,WIDTH/2 + 200,20,600,200);
-		image(start,WIDTH/2 + 250,250,400,100);
-		image(upgrade,WIDTH/2 + 250,400,400,100);
+		switch(option)
+		{
+			case '0':
+			{
+				background(moon);
+				image(title,WIDTH/2 + 200,20,600,200);
+				image(start,WIDTH/2 + 250,250,menuW,menuH);
+				image(upgrade,WIDTH/2 + 250,400,menuW,menuH);
+				
+				s.run();
+				
+				break;
+			}
+			case '1':
+			{
+				background(255);
+				
+				break;
+			}
+			case '2':
+			{
+				background(0);
+				
+				break;
+			}
+		}
+	}
+	
+	public static void Main(String args[])
+	{
+		PApplet.main(new String[]{ie.dit.Assignment3.class.getName() });
+	}
+	
+	public void mousePressed()
+	{
+		//System.out.println("Pressed");
+		if(mouseX > (WIDTH/2 + 250) && mouseX < (WIDTH/2 + 250) + menuW )
+		{
+			if(mouseY > 250 && mouseY < 250 + menuH)
+			{
+				//p.option = '1';
+				System.out.println("Pressed");
+			}
+			//p.option = '1';
+			//System.out.println("Pressed");
+		}
+		if(mouseX > (WIDTH/2 + 250) && mouseX < (WIDTH/2 + 250) + menuW )
+		{
+			if(mouseY > 400 && mouseY < 400 + menuH)
+			{
+				//p.option = '1';
+				System.out.println("Pressed2");
+			}
+			//p.option = '1';
+			//System.out.println("Pressed");
+		}
 	}
 }
