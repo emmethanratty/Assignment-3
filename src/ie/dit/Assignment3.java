22645d9eaa4c801061731d48366ef0ce914f0965
emmethanratty <c13503217@mydit.ie>
2015-04-10 13:26:41 +0100

    Added Gravity, Acc, Dist

--- src/ie/dit/Assignment3.java
+++ src/ie/dit/Assignment3.java
@@ -21,6 +21,8 @@ public class Assignment3 extends PApplet{
 	
 	int menuW = 400;
 	int menuH = 100;
+	
+	float counter;
 			
 	public void setup()
 	{
@@ -34,6 +36,8 @@ public class Assignment3 extends PApplet{
 		 upgrade = loadImage("Upgrade.png");
 		 stage = loadImage("Stage.png");
 		 spaceman = loadImage("Spaceman.png");
+		 
+		 counter=0.0f;
 		 
 	}
 
@@ -137,89 +141,195 @@ public class Assignment3 extends PApplet{
 	  p = new Man(playerXML);
 	}
 
-class Man
-{
-  PVector pos;
-  char up;
-  char down;
-  char left;
-  char right;
-  char start;
-  char button1;
-  char button2;
-  
-  int gHeight = 420;
-  
-  float PH = 200;
-  float PW = 200;
-  float speed = 2;
-  
-  
-  char option = '0';
-    
-  Man()
-  {
-	pos = new PVector(width / 2, height / 2);
-    pos.y = gHeight;
-    pos.x = PW/2;
-  }
-  
-  Man( char up, char down, char left, char right, char start, char button1, char button2)
-  {
-    this();
-    this.up = up;
-    this.down = down;
-    this.left = left;
-    this.right = right;
-    this.start = start;
-    this.button1 = button1;
-    this.button2 = button2;
-  }
-  
-  Man(XML xml)
-  {
-    this(
-          buttonNameToKey(xml, "up")
-        , buttonNameToKey(xml, "down")
-        , buttonNameToKey(xml, "left")
-        , buttonNameToKey(xml, "right")
-        , buttonNameToKey(xml, "start")
-        , buttonNameToKey(xml, "button1")
-        , buttonNameToKey(xml, "button2")
-        );
-  }
-  
-  
-  
-  
-  
-  public void run()
-  {
-	  display();
-	  move();
-  }
-  
-  public void display()
-  {
-	  image(spaceman,pos.x,pos.y,PH,PW);
-  }
-  public void move()
-  {
-	if(checkKey(left))
+	class Man
 	{
-    if(pos.x > 0)
-    {
-       pos.x -= speed;
-    }
+	  PVector pos;
+	  char up;
+	  char down;
+	  char left;
+	  char right;
+	  char button1;
+	  char button2;
+	  
+	  boolean nofloor;
+	  boolean nojump;
+	  boolean falling;
+	  
+	  int gHeight = 420;
+	  int moveS = 3;
+	  
+	  float PH = 200;
+	  float PW = 200;
+	  float PX = 50;
+	  float PY;
+	  float gravityF = 5;
+	  float currentJH = 400;
+	  float jumpH = currentJH;
+	  float rollspeed = 4;
+	  float distance = 0;
+	  float Cacc = 300f;
+	  float acc = Cacc;
+	  float Gdis;
+	  
+	  //char option = '0';
+	    
+	  Man()
+	  {
+	    pos = new PVector(width / 2, height / 2);
+	    pos.y = gHeight;
+	    pos.x = width/2;
+	  }
+	  
+	  Man( char up, char down, char left, char right, char button1, char button2)
+	  {
+	    this();
+	    this.up = up;
+	    this.down = down;
+	    this.left = left;
+	    this.right = right;
+	    this.button1 = button1;
+	    this.button2 = button2;
+	  }
+	  
+	  Man( XML xml)
+	  {
+	    this(
+	          buttonNameToKey(xml, "up")
+	        , buttonNameToKey(xml, "down")
+	        , buttonNameToKey(xml, "left")
+	        , buttonNameToKey(xml, "right")
+	        , buttonNameToKey(xml, "button1")
+	        , buttonNameToKey(xml, "button2")
+	        );
+	  }
+	  
+	  public void run()
+	  {
+	    update();
+	    display();
+	    gravity(); 
+	    stopped();
+	    
+	    jumpH = currentJH;
+	  }
+	  
+	  
+	  public void update()
+	  {
+	    if (checkKey(up))
+	    {
+	      if(option == '1')
+	      {
+	         if(pos.y > jumpH && nofloor == false)
+	          {        
+	               pos.y -= 5;
+	          } 
+	          else
+	          {
+	            nojump = false;
+	            nofloor = true;
+	          } 
+	      }
+	    }
+	    else
+	    {
+	      if(option == '1')
+	      {
+	        //checks ti make sure player is still on screen
+	        if(pos.y < gHeight - PH/2)
+	          {        
+	               nofloor = true;
+	          } 
+	      }
+	    }
+	    if (checkKey(left))
+	    {
+	      if(falling == false)
+	      {
+	        //checks ti make sure player is still on screen
+	        if(pos.x > 0)
+	        {
+	          pos.x -= moveS;
+	          rollspeed = 2 *(acc/100);
+	          distance -= .1f;  
+	        }
+	      }
+	    }
+	    //changes rollspeed back to normal
+	    else
+	    {
+	      rollspeed = 4*(acc/100);
+	    }    
+	    if (checkKey(right))
+	    {
+	      if(falling == false)
+	      {
+	        if(pos.x < width)
+	        {
+	          pos.x += moveS;
+	          rollspeed = 6*(acc/100);
+	          distance += .1f;
+	        }
+	      }
+	    }
+	    //changes rollspeed back to normal
+	     else
+	    {
+	      rollspeed = 4*(acc/100);
+	    }   
+	  }
+
+	  //displays text and images  
+	  public void display()
+	  { 
+	    textSize(30);
+	    text("Distance: " + distance,30,50);
+	    text("Acceleration: " + acc,30,100);
+	    
+	     image(spaceman,pos.x - PW/2,pos.y,PW,PH);   
+	     /*
+	     translate(pos.x, pos.y);
+	       
+	     rotate(counter*TWO_PI/360);
+	     
+	     translate(-PW/2, -PW/2);
+	       
+	     image(spaceman,0,0,PW,PH);
+	     
+	     counter += rollspeed *(acc/100);
+	     */
+	  } 
+	 
+	  //makes gravity
+	  public void gravity()
+	  {
+	     if(nofloor)
+	     {
+	        pos.y += gravityF; 
+	     }
+	     if(pos.y == gHeight)
+	     {
+	       nofloor = false;
+	     }   
+	   }
+	   
+	   //when stopped
+	   public void stopped()
+	   {
+	     if(option == '1')
+	     {
+	    	System.out.println("Moving");
+	       distance += .2f*(acc/100);
+	       acc -= .1f;
+	       // checks to see if stopped
+	       if( acc < 0 )
+	       {
+	          option = '3';
+	         Gdis = distance;
+	         distance= 0f;
+	         acc = Cacc; 
+	       }
+	     }
+	   }
 	}
-    
-    if (checkKey(right))
-    {
-          pos.x += speed;
-    }
-  
-  
-  
- }
-}
 }
